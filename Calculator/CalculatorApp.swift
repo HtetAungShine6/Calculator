18758a5a44d8efebd1aa818066650ef057dcd369
Htet Aung Shine <103210228+HtetAungShine6@users.noreply.github.com>
2023-06-27 21:47:31 +0700

    initial commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Akito Daiki on 15/5/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
